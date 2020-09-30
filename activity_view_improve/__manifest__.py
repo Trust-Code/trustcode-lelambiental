83a45213fb1692c000492f7dc3d6e4b89151f962
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-09-30 11:45:47 -0300

    Novo modulo para acessar rapidamente o objeto da atividade

---
+++ activity_view_improve/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Melhorias nas atividades',
+    'version': '10.0.1.0.0',
+    'category': "Sales",
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+        'br_sale',
+    ],
+    'data': [
+        'views/mail_activity.xml',
+    ],
+}
---
+++ activity_view_improve/models/activity.py
@@ -0,0 +1,17 @@
+
+from odoo import models
+
+
+class MailActivity(models.Model):
+    _inherit = 'mail.activity'
+    
+    def action_view_object(self):
+        return {
+            'name': 'Atividades',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': self.res_model,
+            'res_id': self.res_id,
+            'views': [(False, 'form')],
+            'type': 'ir.actions.act_window',
+        }
